b54b377266082c02f1ad58845dea264f00a45e82
andres-tec <andrestecpile97@gmail.com>
2024-10-13 20:15:47 -0600

    Feat: how to give format to a JTextField

--- javaswing/src/javaswing/SwingComponents.java
+++ javaswing/src/javaswing/SwingComponents.java
@@ -3,31 +3,42 @@ package javaswing;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.*;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.swing.*;
+import javax.swing.text.*;
 
 
 
-class MyFrame extends JFrame implements ActionListener
+class MyFrame extends JFrame
 {
-	JLabel l;
-	JButton b; 
-	int count =0; 
+
 	MyFrame(){
-		super("Swing Demo");
+		JTextField tf1= new JTextField(14);
+		DateFormat df = new SimpleDateFormat("dd/MMMM/yyyy");
+		JFormattedTextField tf2 = new JFormattedTextField(df);
+		tf2.setColumns(15);
+		tf2.setValue(new Date());
+		
+//		NumberFormat nf = NumberFormat.getInstance();
+		NumberFormat nf = NumberFormat.getCurrencyInstance(Locale.US);
+		NumberFormatter nft= new NumberFormatter(nf);
+		nft.setAllowsInvalid(false);
+//		nft.setMaximum(20);
+		JFormattedTextField tf3 = new JFormattedTextField(nft);
+		tf3.setColumns(15);
+		tf3.setValue(20);  
 		setLayout(new FlowLayout());
-		l= new JLabel("Clicked " + count + " Times" );
-		b= new JButton("Clicked");
-		b.addActionListener(this);
-		getRootPane().setDefaultButton(b);
-		add(l);
-		add(b);
-	}
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		count++;
-		l.setText("Clicked " + count + " Times" );
+		
+		JTextArea txtArea= new JTextArea(20,30);
+		
+		add(tf1);
+		add(tf2);
+		add(tf3);
+		add(txtArea);
+		
 	}
 }
 
